6dc16249b835b093cdf879b812e282f472ebacdf
Zachary Hill <zahillmtu@gmail.com>
2022-12-02 19:22:31 -0500

    Updating day1 for part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,15 +1,38 @@
+def find_max_calories(f):
+    max_calories = [0, 0, 0]
 
-f = open("input.txt", "r")
+    current_calories = 0
+    for line in f:
+        if line.isspace():
+            if current_calories > max_calories[2]:
+                insert_new_max(max_calories, current_calories, 2)
 
-maxCalories = -100
+            current_calories = 0
+        else:
+            current_calories += int(line)
 
-currentCalories = 0
-for line in f:
-    if line.isspace():
-        if currentCalories > maxCalories:
-            maxCalories = currentCalories
-        currentCalories = 0
-    else:
-        currentCalories += int(line)
+    print("Single elf: " + str(max_calories[0]))
+    print("Top three elves: " + str(sum(max_calories)))
 
-print(maxCalories)
+
+def insert_new_max(max_calories, new_value, index_to_check):
+    if index_to_check < 0 or index_to_check > 2:
+        return
+
+    if max_calories[index_to_check] > new_value:
+        return
+
+    # Swap the values down
+    temp = max_calories[index_to_check]
+    max_calories[index_to_check] = new_value
+
+    if index_to_check + 1 <= 2:
+        max_calories[index_to_check + 1] = temp
+
+    if index_to_check - 1 < 0:
+        return
+
+    insert_new_max(max_calories, new_value, index_to_check - 1)
+
+
+find_max_calories(open("input.txt", "r"))
